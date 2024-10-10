fc67701f1dd1ff4992814868101e9c099ceb7b80
WaltCSZ <1420970368@qq.com>
2024-10-11 00:33:02 +0800

    basically finish

---
+++ autoCheckElf.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+libcdir="/glibc-all-in-one/"
+patchCmd="pelf"
+work_location="$(pwd)"
+lineChr='-'
+# echo $0
+# echo $1
+# echo $2
+
+if [ $# -gt 2 ]; then
+	echo 'Up to 2 parameter!'
+	exit
+fi
+
+# check elf secure 
+if [ $# -ge 1 ]; then
+    elfName=$1
+else
+    elfName="pwn"
+fi
+
+drawLine(){
+	local ScreenLen=$(stty size |awk '{print $2}')
+	local TitleLen=$(echo -n $1 |wc -c)
+	local LineLen=$(((${ScreenLen} - ${TitleLen}) / 2 ))
+	yes ${lineChr} |sed ''''${LineLen}'''q' |tr -d "\n" && echo -n $1 && yes ${lineChr} |sed ''''${LineLen}'''q' |tr -d "\n" && echo
+
+}
+getGadget(){
+    local rfile="$1"
+    drawLine "Gadgets in $2"
+    ROPgadget --binary "./$rfile" --only 'syscall|pop|ret' | grep -v "ret .*" |grep -E "syscall|rdi|rsi|rdx|rcx|r10|r8|r9|: ret"   
+    
+}
+
+drawLine "ELF Check"
+if [ -f ${elfName} ]; then
+    printf "[+] ELF Path: %s\n" "$(dirname $elfName)/$(basename $elfName)"
+    checksec --file "$elfName" 2>&1
+    # elf_info=$(checksec --file $elfName 2>&1 | tee /dev/tty)
+    # elf_arch=$(echo "$elf_info" | grep -oP "Arch:     \K[^-]*")
+    # printf "arch: %s\n" "$elf_arch"
+    
+else
+    echo "No find the ELF file!"
+    exit
+fi
+
+
+
+# check libc version
+if [ ! -z $2 ] && [ -e $2 ]; then
+    libc_path_list=(`find "$2" -type f -name "libc-*.so" -o -name "libc.so.6"`)
+else
+    libc_path_list=(`find . -type f -name "libc-*.so" -o -name "libc.so.6"`)
+fi
+# libc_path_list=$( ls -l ${work_location} | grep "libc.*")
+# libc_path_list=($( find . -type f -name "libc-*.so" -o -name "libc.so.6" ))
+
+drawLine "Libc Check"
+echo -ne "\e[37m"
+printf "[+] Libc Found: \n"
+for i in ${!libc_path_list[@]};do
+    libc_path=${libc_path_list[$i]}
+    lib_ver=$(strings $libc_path | grep -oP 'Library \(\K[^)]*')
+    lib_ver_list[$i]=${lib_ver}
+    printf " [%d] %s \t%s\n" "$[i+1]" "$lib_ver" "$libc_path"
+done
+echo -ne "\e[0m"
+# for i in ${!lib_ver_list[@]};do
+#     printf "%d:%s\n" $i "${lib_ver_list[$i]}"
+# done
+
+# function getlib(){
+#     local slibv=("$@")
+#     plibc=()
+#     for i in ${!slibv[@]};do
+
+#         local obj=${slibv[$i]##* }
+#         echo "${libcdir}/${obj}_${arch}"
+#         if [ -d ${libcdir}/${obj}_${arch} ];then
+#             plibc[${#plibc_path_list[@]}]=${obj}_${arch}
+#         fi
+#     done
+# }
+
+# echo "start"
+# getlib "${lib_ver_list[@]}"
+# echo "end"
+# for i in ${!plibc[@]};do
+#     printf "%d:%s\n" $i "${plibc[$i]}"
+# done
+
+if [ ${#libc_path_list[@]} -gt 0 ]; then
+    printf "Find \33[31m%d\33[0m Libc in current path.\n" ${#libc_path_list[@]}
+    if [ ${#libc_path_list[@]} -gt 1 ]; then
+        # echo "Choose a libc to use:"
+        # select libc_path in ${libc_path_list[@]}
+        read -t 5 -p "Switch the libc_path: [1-${#libc_path_list[@]}]" answer
+        if [ -z $answer ]; then
+            echo "\nTimeout! [1] was selected."
+            answer=1
+        else
+            echo "[${answer}] was selected."
+            
+        fi
+        if [ $answer -gt ${#libc_path_list[@]} ] || [ $answer -le 0] ; then
+            echo " [+] Out of range!"
+            exit 1
+        else
+            answer=$[answer-1]
+            libc_path=${libc_path_list[$answer]}
+            lib_ver=${lib_ver_list[$answer]}
+        fi
+    else
+        libc_path=${libc_path_list[0]}
+        lib_ver=${lib_ver_list[0]}
+    fi
+    
+else
+    echo "Libc not found."    
+fi
+
+printf "\n[+] Libc Info:\n  Version:\t\33[92m%s\33[0m\n  Libc Path:\t%s\n\n" "${lib_ver}" "${libc_path}"
+
+# get gadget which most commonly used 
+getGadget "$elfName" "ELF"
+getGadget "$libc_path" "${libc_path##*/}"
+drawLine "End of gadgets"
+
+# patch libc
+drawLine "Patch ELF"
+read -t 5 -n1 -p "Patch libc to the elf? [Y/n]" answer
+case $answer in
+    N|n)
+        echo "Don't patch."
+        ;;
+    *)
+        echo "To Patch."
+        # lib=${libc_path##* }
+        # if [ -d ${libcdir}/${lib}_${arch} ];then
+        if [ -n "$libc_path" ]; then
+            # echo "with fiel"
+            "${patchCmd}" "$elfName" "$libc_path"
+        else
+            # echo "no libc"
+            "${patchCmd}" "$elfName"
+        fi
+        ;;
+        # getlib "${lib_ver_list[@]}"
+        # if [ ${#plibc_path_list[@]} -ge 1 ]; then
+        #     echo "Choose a libc:"
+        #     select obj in ${plibc_path_list[@]} "others"; do
+        #         if [ "$obj" != "others" ]; then
+                    
+        #             "${patchCmd}" "$elfName" "$obj"
+        #             break
+        #         else
+        #             "${patchCmd}" "$elfName"
+        #             break
+        #         fi
+        #     done
+        # else
+        #     "${patchCmd}" "$elfName"
+        # fi
+        # ;;
+esac
+echo 
+
+
+# getGadget "$elfName"
+# echo "${libc_path_list[0]}"
+# getGadget "${libc_path_list[0]}"
+
+
+# check seccomp
+drawLine "Check Sandox"
+read -t 5 -n1 -p "Check sandbox? [Y/n]" answer
+case $answer in
+    N|n)
+        echo "Don't check sandbox."
+        ;;
+    *)
+        echo "To check sandbox."
+        seccomp-tools dump "./$elfName"
+        ;;
+esac
+echo "end"
+
---
+++ autoPatchElf.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+aioDir="/glibc-all-in-one/"
+
+lineChr='-'
+
+# help
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+	cmd=$(basename "$0")
+	echo 'Patchelf with glibc-all-in-one. '
+	echo -e "Usage:\t${cmd} elf [lib|path|ver]"
+	echo 'Example:'
+	echo -e "\t${cmd} pwn libc.so.6"
+	echo -e "\t${cmd} pwn libc/"
+	echo -e "\t${cmd} pwn 2.35"
+	exit 1
+fi
+
+drawLine(){
+	local ScreenLen=$(stty size |awk '{print $2}')
+	local TitleLen=$(echo -n $1 |wc -c)
+	local LineLen=$(((${ScreenLen} - ${TitleLen}) / 2 ))
+	yes ${lineChr} |sed ''''${LineLen}'''q' |tr -d "\n" && echo -n $1 && yes ${lineChr} |sed ''''${LineLen}'''q' |tr -d "\n" && echo
+
+}
+# Check elf legal
+elfName="$1"
+if [ ! -f "$elfName" ]; then
+	echo -e "\33[31m[!] ELF file is illegal.\33[0m"
+	exit 1
+fi
+printf "[+] Patch ELF: %s\n" "$(dirname $elfName)/$(basename $elfName)"
+# check whether the patched elf existed.
+fname="${elfName}_pe"
+if [ -f ${fname} ]; then
+	echo -e '[!] The file has been \e[31mPATCHED\e[0m a LIB.'
+	drawLine "Patched Info"
+	echo -ne "\e[37m"
+	ldd "${fname}"
+	echo -ne "\e[0m"
+	drawLine
+	printf "\33[34m[?]\33[0m "
+	if ! read -t 5 -n1 -p "Do you want to overwriting? [Y/n]" answer; then
+		echo ' Timeout'
+		answer='Y'
+	fi
+	case $answer in
+		N|n)
+			echo '[+] Quit'	
+			exit 2
+			;;
+		*)
+			echo '[-] Will be overwritten. '
+			echo 
+			;;
+	esac
+fi
+
+# get lib version from libc.so.6
+extractLib() {
+	local libcFile="$1"
+	local elfArch=$(checksec --file $elfName 2>&1 | grep -oP "Arch:     \K[^-]*")
+	local libVersion=$(strings "$libcFile" | grep -oP 'Library \(\K[^)]*')
+	local libVer=${libVersion##* }
+	if [ -z "$elfArch" ] || [ -z "$libVer" ]; then
+		echo -e "\33[31m[!] Error: Fault in extract libc infomation.\33[0m"
+		exit 1
+	fi
+	libInfo="${libVer}_${elfArch}"
+}
+
+# check lib path then load
+checkLib() {
+	local path="$1"
+	if [ ! -d "$path" ]; then
+		return 1
+	fi
+	local ldList=(`find "$path" -type f -name "ld-*.so" -o -name "ld-linux*.so.2"`)
+	if [ ${#ldList[@]} -eq 0 ]; then
+		return 2
+	fi
+	local libcList=(`find "$path" -type f -name "libc-*.so" -o -name "libc.so.6"`)
+	if [ ${#libcList[@]} -eq 0 ]; then
+		return 3
+	fi
+	libPath="$path"
+	ldPath="${ldList[0]}"
+	libcPath="${libcList[0]}"
+	return 0
+}
+
+
+aioDir=${aioDir%/}
+if [ ! -z $2 ]; then
+	argLib="$2"
+	if [ -f "$argLib" ]; then
+		extractLib "$argLib"
+		printf "[+] Libc Info: \33[93m%s\33[0m -- %s\n" "${libInfo}" "$(dirname $argLib)/$(basename $argLib)"
+		checkLib "$(dirname '${argLib}')"
+		if [ $? -gt 0 ]; then
+			echo -e "\e[31m[!]\e[0m No match ld. So match in the LIBs. "
+			checkLib "${aioDir}/${libInfo}"
+			if [ $? -gt 0 ]; then 
+				echo -e "\e[31m[!]\e[0m There is no match in the LIBs. "
+			fi
+		fi
+	elif [ -d "$argLib" ]; then
+		checkLib "$argLib"
+		if [ $? -gt 0 ]; then 
+			echo -e "\e[31m[!]\e[0m The LIB Path is error. "
+		fi
+		extractLib "${libcPath}"
+	else
+		libFilt="${argLib}"
+	fi
+fi
+
+if [ -z $libPath ]; then
+	aioList=$( ls -l "${aioDir}/" | grep '^d' | cut -c 44- )
+	if [ ! -z "$libFilt" ]; then
+		filtRes=$( echo "$aioList" | grep "$libFilt" )
+		if [ ${#filtRes[@]} -gt 0 ]; then
+			aioList="${filtRes[@]}"
+		else
+			echo "\e[31m[!]\e[0m Lib version can not match."
+		fi
+	fi
+	echo -e "\e[34m[?]\e[0m Choose a LIB to PATCH:"
+	select obj in ${aioList[@]} "Quit"; do
+		if [ "$obj" != "Quit" ]; then
+			# echo "Patch elf"
+			# echo "$1: ${obj}"
+			
+			# libc=${aioDir}${obj}
+			# ld=($(ls -d ${libc}/* | grep "ld-.*\.so"))
+
+			# if [ ${#ld} = 0 ]; then
+			# 	echo 'error'
+			# 	exit
+			# fi
+			libInfo="${obj}"
+			path="${aioDir}/${obj}"
+			checkLib "$path"
+			break
+		else
+			echo "[+] Quit."
+			exit 2
+		fi
+	done
+fi
+
+# patch process
+if [ -n "$libPath" ] && [ -n "$libcPath" ] && [ -n "$ldPath" ]; then
+	echo "Patching..."
+	if [ -f ${fname} ]; then
+		rm -f "${fname}"
+	fi
+	cp "$1" "${fname}"
+	patchelf --set-interpreter "${ldPath}" ${fname}
+	ldRes=$?
+	#patchelf --replace-needed libc.so.6 "${libc}/libc.so.6" ${fname}
+	patchelf --set-rpath "${libPath}" ${fname}
+	rpRes=$?
+	if [ $ldRes -eq 0 ] && [ $rpRes -eq 0 ]; then
+		echo '[+] Done!'
+		printf "Last Patch:\t\33[92m%s\33[0m \nLib Path:\t%s\n" "${libInfo}" "${libPath}/"
+		exit 0
+	else
+		echo 'Error: Fault in patch'
+		exit 1
+	fi
+else
+	echo "Error: No such File."
+	exit 1
+fi
+
